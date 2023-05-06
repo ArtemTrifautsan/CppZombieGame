c596f32f3d23aadbdac1d0d05ccee6f99812197a
ArtemTrifautsan <trifautsan01@mail.ru>
2023-05-06 13:14:48 +0600

    [feat]: add timer object
    The timer will be used to control the FPS and in other tasks.

--- src/other.cpp
+++ src/other.cpp
@@ -11,16 +11,34 @@ SDL_Texture* loadTexture(SDL_Renderer* renderer, char* path)
 
 void drawTexture(SDL_Renderer* renderer, SDL_Texture* texture, int x, int y, SDL_RendererFlip flip, float resize)
 {
-    SDL_Rect dest1;
-    dest1.x = 0;
-    dest1.y = 0;
-    SDL_QueryTexture(texture, NULL, NULL, &dest1.w, &dest1.h);
+    SDL_Rect srcRest;
+    srcRest.x = 0;
+    srcRest.y = 0;
+    SDL_QueryTexture(texture, NULL, NULL, &srcRest.w, &srcRest.h);
 
-    SDL_Rect dest2;
-    dest2.x = x;
-    dest2.y = y;
-    dest2.w = (int)dest1.w * resize;
-    dest2.h = (int)dest1.h * resize;
+    SDL_Rect dstRest;
+    dstRest.x = x;
+    dstRest.y = y;
+    dstRest.w = (int)srcRest.w * resize;
+    dstRest.h = (int)srcRest.h * resize;
 
-    SDL_RenderCopyEx(renderer, texture, &dest1, &dest2, 0, NULL, flip);
+    SDL_RenderCopyEx(renderer, texture, &srcRest, &dstRest, 0, NULL, flip);
+}
+
+Timer::Timer() { this->startTime = -1; }
+
+void Timer::start() { this->startTime = SDL_GetTicks(); }
+
+void Timer::stop() { this->startTime = -1; }
+
+void Timer::sleep(int ms) { SDL_Delay(ms); }
+
+int Timer::getCurrentTime() { return (SDL_GetTicks() - this->startTime); }
+
+int Timer::getStartTime() { return this->startTime; }
+
+bool Timer::isRunning()
+{
+    if (this->startTime == -1) return false;
+    else return true;
 }
--- src/other.h
+++ src/other.h
@@ -1,11 +1,25 @@
 #ifndef OTHER_H
 #define OTHER_H
 
+
 #include <SDL.h>
 #include <SDL_image.h>
 
 SDL_Texture* loadTexture(SDL_Renderer* renderer, char* path);
 void drawTexture(SDL_Renderer* renderer, SDL_Texture* texture, int x, int y, SDL_RendererFlip flip, float resize);
+
+class Timer {
+public:
+	Timer();
+	void start();
+	void stop();
+	void sleep(int ms);
+	int getCurrentTime();
+	int getStartTime();
+	bool isRunning();
+private:
+	int startTime;
+};
 
 
 #endif
